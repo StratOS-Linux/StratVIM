7312a4e08d877c0614c2d0c8fc492ad0c934c277
Slipstream <hatkardhruv@gmail.com>
2024-02-09 13:55:02 +0530

    Added tdo.nvim for todo notes

---
+++ lua/plugins/tdo.lua
@@ -0,0 +1,6 @@
+return{
+        '2kabhishek/tdo.nvim',
+        dependencies =  'nvim-telescope/telescope.nvim',
+        cmd = { 'Tdo', 'TdoEntry', 'TdoNote', 'TdoTodos', 'TdoToggle', 'TdoFind', 'TdoFiles' },
+        keys = { '[t', ']t' },
+}
