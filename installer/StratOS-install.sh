efcef917423dd37d3467b1a916e107352258155d
Oleksandr Yizchak Sanin <alexaaander.sanin@gmail.com>
2024-10-07 16:56:36 +0300

    Updates:
    1 Error Handling: Added set -e to ensure the script exits immediately if any command fails.
    2 Consistent Formatting: Improved indentation and spacing for better readability.
    3 Main Function: Encapsulated the main logic in a main function for better structure and potential future expansion

--- installer/StratOS-install.sh
+++ installer/StratOS-install.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+set -e
+
 install_dependencies() {
-	sudo pacman -S neovim lazygit cargo npm nodejs python python-pywal xclip --noconfirm 
-	echo -e "$(tput setaf 2 bold)Dependencies for StratVIM are installed.$(tput sgr 0)"
+    sudo pacman -S --noconfirm neovim lazygit cargo npm nodejs python python-pywal xclip
+    echo -e "$(tput setaf 2 bold)Dependencies for StratVIM are installed.$(tput sgr 0)"
 }
 
-install_stratvim(){
-	git clone https://github.com/StratOS-Linux/StratVIM.git ~/.config/nvim
-	echo -e "$(tput setaf 2 bold)StratVIM is installed! You can now use StratVIM by typing 'nvim' in the terminal.$(tput sgr 0)"
+install_stratvim() {
+    git clone https://github.com/StratOS-Linux/StratVIM.git ~/.config/nvim
+    echo -e "$(tput setaf 2 bold)StratVIM is installed! You can now use StratVIM by typing 'nvim' in the terminal.$(tput sgr 0)"
 }
 
-install_dependencies
-install_stratvim
+main() {
+    install_dependencies
+    install_stratvim
+}
+
+main
